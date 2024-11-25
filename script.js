19c5c6f848800df795dbf33c7309d04a31a31acd
sykes <kanekikenrus@mail.ru>
2024-11-25 13:26:04 +0500

    add simple logic

--- script.js
+++ script.js
@@ -1,22 +1,35 @@
 const add = function(num1, num2){
-    return num1 + num2
-}
+    return +num1 + +num2
+};
 
 const subtract = function(num1, num2){
     return num1 - num2
-}
+};
 
 const multiply = function(num1, num2){
     return num1 * num2
-}
+};
 
 const divide = function(num1, num2){
     return num1 / num2
-}
+};
 
-const firstValue = promt();
-const secondValue = promt();
-console.log(add(firstValue, secondValue))
-console.log(subtract(firstValue, secondValue))
-console.log(multiply(firstValue, secondValue))
-console.log(divide(firstValue, secondValue))
+const operate = function(statement, firstValue, secondValue){
+    if (statement === '+') {
+        return add(firstValue, secondValue)
+    }
+    else if (statement === '-') {
+        return subtract(firstValue, secondValue)
+    }
+    else if (statement === '*') {
+        return multiply(firstValue, secondValue)
+    }
+    else if (statement === '/') {
+        return divide(firstValue, secondValue)
+    }
+};
+
+const firstValue = prompt('pass the first number');
+const secondValue = prompt('pass the second number');
+const statement = prompt('pass the operator');
+console.log(operate(statement, firstValue, secondValue));
