e6e3d135d11943e98f19b9d23619d796e03ffd34
sykes <kanekikenrus@mail.ru>
2024-11-26 10:28:51 +0500

    calculator version 1.0

--- script.js
+++ script.js
@@ -28,8 +28,63 @@ const operate = function(statement, firstValue, secondValue){
         return divide(firstValue, secondValue)
     }
 };
+let valueHolder;
+let firstValue;
+let secondValue;
+let statement;
 
-// const firstValue = prompt('pass the first number');
-// const secondValue = prompt('pass the second number');
-// const statement = prompt('pass the operator');
-// console.log(operate(statement, firstValue, secondValue));
+const buttonsNumbers = document.querySelectorAll(".number");
+buttonsNumbers.forEach((button) => {
+    button.addEventListener("click", fillTheField);
+});
+
+function fillTheField(e) {
+    const display = document.querySelector('.display')
+    if (display.textContent === '0' || display.textContent === '/' || display.textContent === '*' || display.textContent === '-' || display.textContent === '+') {
+        display.textContent = ''
+    }
+    const buttonRef = e.target
+    const number = buttonRef.getAttribute('data-value')
+    // console.log(number)
+    display.textContent += number
+    valueHolder = display.textContent
+    console.log(valueHolder)
+}
+
+const ac = document.querySelector('.ac')
+ac.addEventListener("click", () => {
+    const display = document.querySelector('.display')
+    display.textContent = '0'
+    valueHolder = display.textContent
+    console.log(valueHolder)
+});
+
+const statements = document.querySelectorAll(".statement");
+statements.forEach((statement) => {
+    statement.addEventListener("click", getStatement);
+});
+
+function getStatement(e) {
+    const display = document.querySelector('.display')
+    firstValue = display.textContent;
+    const statementRef = e.target
+    const statementFunc = statementRef.getAttribute('data-value')
+    statement = statementFunc
+    
+    display.textContent = statementFunc
+    
+}
+
+const theOperate = document.querySelector(".operate");
+theOperate.addEventListener('click', getTheOperate);
+
+function getTheOperate(e) {
+    secondValue = valueHolder;
+    const finalRes = operate(statement, firstValue, secondValue)
+    const display = document.querySelector('.display')
+    console.log(firstValue)
+    console.log(statement)
+    console.log(secondValue)
+    display.textContent = `${finalRes}`
+    firstValue = finalRes;
+}
